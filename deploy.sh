b8bb56204a5d82685585a7b00e857fcb458b2c49
natalya tomova <natalyayurevna19@gmail.com>
2024-02-12 15:54:39 +1000

    Изменен на запуск в docker

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-python3 -m venv env
-source env/bin/activate
-pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic  --no-input
-deactivate
+#python3 -m venv env
+#source env/bin/activate
+#pip3 install -r requirements.txt
+#python3 manage.py migrate
+#python3 manage.py collectstatic  --no-input
+#deactivate
+docker-compose up --build -d
